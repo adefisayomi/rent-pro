8c25f86f41e347cbd20b635e8db86575eb52c087
dolapo oluwole <adefisayomioluwole@gmail.com>
2025-07-18 11:11:16 +0100

    added a loading ui for all pages

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,11 +1,16 @@
+"use client"
+
 import Logo from "@/components/Logo";
+import { usePathname } from "next/navigation";
 
 export default function Loading() {
+  const pathname = usePathname().slice(1).replaceAll('/', '-');
   return (
-    <div className="fixed flex items-center justify-center top-0 left-0 w-full h-[100vh] bg-slate-50">
+    <div className="fixed flex items-center justify-center flex-col gap-3 top-0 left-0 w-full h-[100vh] bg-slate-50">
       <div className="animate-blink">
         <Logo/>
       </div>
+      <p className="text-muted-foreground font-medium text-[10px] capitalize">{pathname}</p>
     </div>
   );
 }
